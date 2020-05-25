77a5bd7e04f59822ee84edffa68518ede034760d
Vogrim <Fox210289@mail.ru>
2020-05-25 23:50:25 +0300

    Завершение адаптивной графики

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,18 +1,13 @@
 var link = document.querySelector(".form__submit");
 var close = document.querySelector(".modal__btn");
 var form = document.querySelector(".form");
-var popup = document.querySelector(".page__main .modal--success");
-var popuperror = document.querySelector(".page__main .modal--error");
+var popup = document.querySelector(".modal--success");
+var popuperror = document.querySelector(".modal--error");
 var login = form.querySelector("[name=login]");
 var family = form.querySelector("[name=family]");
 var tel = form.querySelector("[name=tel]");
 var email = form.querySelector("[name=mail]");
 var submitBtn = form.querySelector("[type=submit]");
-
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal--on");
-});
 
 submitBtn.addEventListener("click", function (evt) {
   if (!login.value || !family.value || !email.value ||!tel.value) {
@@ -23,6 +18,8 @@ submitBtn.addEventListener("click", function (evt) {
     popuperror.classList.add("modal--on");
 } else {
   if (isStorageSupport) {
+    evt.preventDefault();
+    popup.classList.add("modal--on");
     localStorage.setItem("login", login.value);
     localStorage.setItem("family", family.value);
     localStorage.setItem("email", email.value);
@@ -33,6 +30,6 @@ submitBtn.addEventListener("click", function (evt) {
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal--on");
   popuperror.classList.remove("modal--on");
+  popup.classList.remove("modal--on");
 });
