47909baa556f906998abe5a429d42d813c77e786
Vogrim <Fox210289@mail.ru>
2020-05-26 02:30:00 +0300

    Исправление замечаний

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,24 +1,31 @@
-var link = document.querySelector(".form__submit");
-var close = document.querySelector(".modal__btn");
 var form = document.querySelector(".form");
 var popup = document.querySelector(".modal--success");
 var popuperror = document.querySelector(".modal--error");
+var close = document.querySelector(".modal__btn");
+var submitBtn = form.querySelector("[type=submit]");
 var login = form.querySelector("[name=login]");
 var family = form.querySelector("[name=family]");
 var tel = form.querySelector("[name=tel]");
 var email = form.querySelector("[name=mail]");
-var submitBtn = form.querySelector("[type=submit]");
+var isStorageSupport = true;
+var storage = "";
 
-submitBtn.addEventListener("click", function (evt) {
-  if (!login.value || !family.value || !email.value ||!tel.value) {
+try {
+  var storage = localStorage.getItem("login");
+} catch (error) {
+  var isStorageSupport = false;
+}
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !family.value || !email.value || !tel.value) {
+    return
+}  {
+  if (isStorageSupport) {
     evt.preventDefault();
     popup.classList.remove("modal--on");
     popuperror.classList.remove("modal--on");
     popuperror.offsetWidth = popup.offsetWidth;
     popuperror.classList.add("modal--on");
-} else {
-  if (isStorageSupport) {
-    evt.preventDefault();
     popup.classList.add("modal--on");
     localStorage.setItem("login", login.value);
     localStorage.setItem("family", family.value);
